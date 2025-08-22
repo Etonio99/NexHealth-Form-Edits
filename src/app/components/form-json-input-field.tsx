9e48725ccfc6b981ea17ebccfa44bc38dc00cb85
Etonio99 <ebreiny@gmail.com>
2025-08-21 22:06:36 -0600

    Added an option to force refresh the JSON input data

--- src/app/components/form-json-input-field.tsx
+++ src/app/components/form-json-input-field.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useFormContext } from "./form-context";
+import { FaArrowTurnUp } from "react-icons/fa6";
 
 export default function FormJsonInputField() {
     const [input, setInput] = useState("");
+    const inputRef = useRef<HTMLTextAreaElement>(null);
 
     const { formData, setFormData } = useFormContext();
 
@@ -28,10 +30,23 @@ export default function FormJsonInputField() {
         return input;
     }
 
+    const forceRefresh = () => {
+        if (!inputRef.current) {
+            return;
+        }
+
+        parseAndSendInput(inputRef.current.value);
+    }
+
     return (
         <div className="mt-8">
-            <label htmlFor="formDataInput" className="text-zinc-500 text-xs">Form Page JSON Input</label>
-            <textarea id="formDataInput" value={getValue()} onChange={e => parseAndSendInput(e.target.value)} placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-500" />
+            <div className="flex justify-between text-zinc-500 text-xs">
+                <label htmlFor="formDataInput">Form Page JSON Input</label>
+                <button onClick={forceRefresh}>
+                    <FaArrowTurnUp />
+                </button>
+            </div>
+            <textarea ref={inputRef} id="formDataInput" value={getValue()} onChange={e => parseAndSendInput(e.target.value)} placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-500" />
         </div>
     );
 }
